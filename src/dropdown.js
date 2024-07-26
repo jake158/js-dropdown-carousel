463f8d2a351c5a55d1d576d2d9f9b8886416ee4e
freeram <kulikovpavel2801@gmail.com>
2024-07-26 09:10:53 -0600

    Add dropdown icons

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,9 +1,13 @@
+import upSymbol from './img/chevron-up.svg';
+import downSymbol from './img/chevron-down.svg';
+
 export default class Dropdown {
   constructor(dropdownWrapper, showOnHover = false) {
     const elements = this.#getElements(dropdownWrapper);
     this.optionsList = elements.optionsList;
-    this.#initializeStyle(elements);
+    this.showButtonIcon = elements.showButtonIcon;
 
+    this.#initializeStyle(elements);
     this.hide();
 
     elements.showButton.addEventListener(
@@ -14,18 +18,32 @@ export default class Dropdown {
 
   #getElements(wrapper) {
     const showButton = wrapper.querySelector('button');
+    const showButtonIcon = document.createElement('img');
+    showButtonIcon.src = downSymbol;
+    showButton.appendChild(showButtonIcon);
+
     const optionsList = wrapper.querySelector('ol');
     const optionButtons = optionsList.querySelectorAll('button');
-    return { wrapper, showButton, optionsList, optionButtons };
+    const optionIcons = optionsList.querySelectorAll('img');
+    return {
+      wrapper,
+      showButton,
+      showButtonIcon,
+      optionsList,
+      optionButtons,
+      optionIcons,
+    };
   }
 
   hide() {
     this.optionsList.style.display = 'none';
+    this.showButtonIcon.src = downSymbol;
     this.visible = false;
   }
 
   show() {
     this.optionsList.style.display = 'flex';
+    this.showButtonIcon.src = upSymbol;
     this.visible = true;
   }
 
@@ -35,7 +53,7 @@ export default class Dropdown {
 
   #initializeStyle(elements) {
     const btnHeight = '35px';
-    const btnPadding = '5px 12px';
+    const btnPadding = '4px 10px';
     const btnBgColor = '#f0f0f0';
     const btnHoverBgColor = '#e0e0e0';
     const btnActiveBgColor = '#d0d0d0';
@@ -50,12 +68,32 @@ export default class Dropdown {
       display: 'flex',
     });
 
+    addStyles(elements.showButton, {
+      height: btnHeight,
+      textWrap: 'nowrap',
+      backgroundColor: btnBgColor,
+      border: `1px solid ${btnBorderColor}`,
+      borderRadius: btnBorderRadius,
+      padding: btnPadding,
+      fontFamily: btnFontFamily,
+      fontSize: btnFontSize,
+      cursor: 'pointer',
+      display: 'flex',
+      alignItems: 'center',
+      gap: '5px',
+    });
+
+    addStyles(elements.showButtonIcon, {
+      height: `calc(${btnHeight} / 2)`,
+      width: 'auto',
+    });
+
     addStyles(elements.optionsList, {
       margin: '0',
       padding: '0',
       position: 'absolute',
       left: '0',
-      top: btnHeight,
+      top: `calc(${btnHeight} + 8px)`,
       overflow: 'auto',
       minWidth: '100%',
       listStyleType: 'none',
@@ -72,17 +110,18 @@ export default class Dropdown {
         textWrap: 'nowrap',
         width: '100%',
         height: btnHeight,
-        textAlign: 'left',
         backgroundColor: btnBgColor,
         border: 'none',
         padding: btnPadding,
         fontFamily: btnFontFamily,
         fontSize: btnFontSize,
         cursor: 'pointer',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '8px',
       });
 
       btn.addEventListener('click', () => {
-        elements.showButton.textContent = btn.textContent;
         this.hide();
       });
 
@@ -103,21 +142,11 @@ export default class Dropdown {
       });
     }
 
-    const maxOptionWidth = elements.optionsList.offsetWidth;
-    const btnWidth = elements.showButton.offsetWidth;
-
-    addStyles(elements.showButton, {
-      height: btnHeight,
-      width: `${Math.max(maxOptionWidth, btnWidth)}px`,
-      textWrap: 'nowrap',
-      textAlign: 'left',
-      backgroundColor: btnBgColor,
-      border: `1px solid ${btnBorderColor}`,
-      borderRadius: btnBorderRadius,
-      padding: btnPadding,
-      fontFamily: btnFontFamily,
-      fontSize: btnFontSize,
-      cursor: 'pointer',
-    });
+    for (const icon of elements.optionIcons) {
+      addStyles(icon, {
+        height: `calc(${btnHeight} / 1.5)`,
+        width: 'auto',
+      });
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import 'modern-normalize';
 import './style.css';
 import Dropdown from './dropdown';
 import Carousel from './carousel';
+import placeholder from './img/placeholder.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const placeholderImgs = document.querySelectorAll('.placeholder-icon');
+  for (const img of placeholderImgs) {
+    img.src = placeholder;
+  }
+
   const dropdowns = document.querySelectorAll('.dropdown-wrapper');
   new Dropdown(dropdowns[0]);
   new Dropdown(dropdowns[1], true);
