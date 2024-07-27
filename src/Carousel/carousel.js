fbc876067362b946eef0923f93f4a51d2df570a2
freeram <kulikovpavel2801@gmail.com>
2024-07-27 00:06:00 -0600

    Begin setting up carousel

--- src/Carousel/carousel.js
+++ src/Carousel/carousel.js
@@ -4,15 +4,30 @@ export default class Carousel {
   constructor(carouselWrapper) {
     this.wrapper = carouselWrapper;
     const images = Carousel.getImageArray(carouselWrapper);
+    this.setUpHTML(images);
   }
 
   static getImageArray(wrapper) {
-    const array = [];
-    for (const img of wrapper.querySelectorAll('img')) {
-      array.push(img);
-    }
-    return array;
+    return [...wrapper.querySelectorAll('img')];
   }
+
+  setUpHTML(images) {
+    this.wrapper.classList.add('carousel-wrapper');
+
+    this.wrapper.innerHTML = `
+    <div class="carousel-tape"></div>
+    `;
+
+    this.tape = this.wrapper.querySelector('.carousel-tape');
+    for (const img of images) {
+      img.classList.add('carousel-image')
+      this.tape.appendChild(img);
+    }
+  }
+
+  next() {}
+
+  previous() {}
 
   cycle() {}
 }
